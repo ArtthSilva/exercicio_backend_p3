107e4a0b63410a604b75a7cd0a1e17b68e0b80c6
ArtthSilva <arthur12pereira1@gmail.com>
2024-02-27 16:44:21 -0300

    fix: corrigindo RequestMapping

--- src/main/java/com/arthurpsilva/exercicios/controllers/ExercicioController.java
+++ src/main/java/com/arthurpsilva/exercicios/controllers/ExercicioController.java
@@ -2,14 +2,16 @@ package com.arthurpsilva.exercicios.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/aluno")
 public class ExercicioController {
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public String listaCliente(){
+    public String getName(){
         return "Arthur";
     }
 }
